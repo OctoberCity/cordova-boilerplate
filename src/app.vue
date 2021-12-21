a794b5c549cbe40386380d34865a6e48971d4597
潘肖男 <panxiaonan@enn.cn>
2021-12-21 13:40:34 +0800

    update: vue/cordova 版本信息获取

--- src/app.vue
+++ src/app.vue
@@ -1,17 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="logo-img" alt="Vue logo" src="./assets/logo-vue.png">
+    <img class="logo-img" alt="Cordova logo" src="./assets/logo-cordova.png">
+    <HelloWorld
+      msg="Welcome to Your Vue/Cordova App"
+      :vue-version="vueVersion"
+      :cordova-platform="cordovaPlatform"
+      :cordova-version="cordovaVersion"
+    />
   </div>
 </template>
 
 <script>
+import vue from 'vue'
 import HelloWorld from './components/hello-world.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      vueVersion: vue.version,
+      cordovaPlatform: '',
+      cordovaVersion: '',
+    }
+  },
+  created() {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+  },
+  methods: {
+    onDeviceReady() {
+      const cordova = window.cordova;
+      this.cordovaPlatform = cordova.platformId;
+      this.cordovaVersion = cordova.version
+    }
   }
 }
 </script>
@@ -24,5 +48,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.logo-img {
+  height: 150px;
 }
 </style>
